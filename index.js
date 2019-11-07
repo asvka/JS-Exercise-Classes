15a95219950fbd691043da32cf76c28ded22fdb7
Anthony M Hernandez <55599397+asvka@users.noreply.github.com>
2019-11-07 14:44:35 -0800

    17 passing

--- index.js
+++ index.js
@@ -41,8 +41,24 @@ class Airplane {
 */
 
 class Person {
-  constructor (name, age)
-  this.stomach = [];
+  constructor (name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach =[];
+  }
+  eat(someFood){
+    if (this.stomach.length < 10){
+      this.stomach.push(someFood);
+    }
+  }
+  poop(){
+    if (this.stomach.length >= 10 ){
+      this.stomach = [];
+    }
+  }
+  toString(){
+    return `${this.name}, ${this.age}`
+  }
 }
 
 /*
@@ -60,6 +76,22 @@ class Person {
 */
 
 class Car {
+  constructor (model, milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons){
+    this.tank += gallons;
+  }
+  drive(distance){
+    this.odometer + distance;
+    this.tank -= (distance / this.milesPerGallon);
+    if (this.tank = 0){
+      return 'I ran out of fuel at ' + this.odometer + ' miles!';
+    }
+  }
 
 }
 
