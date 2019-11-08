d45343fb42a57a8e56d279bdb09f4bb4bd09fe5e
Anthony M Hernandez <55599397+asvka@users.noreply.github.com>
2019-11-07 17:18:12 -0800

    46 passing...

--- index.js
+++ index.js
@@ -157,11 +157,10 @@ class Instructor extends Lambdasian {
   demo(subject){
     return `Today we are learning about ${subject}`
   }
-  grade(student){
+  grade(student, subject){
     return `${student.name} receives a perfect score on ${subject}`
   }
 }
-
 /*
   TASK 5
     - Write a Student class extending Lambdasian.
@@ -177,9 +176,24 @@ class Instructor extends Lambdasian {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-
+class Student extends Lambdasian {
+  constructor (studentAttr) {
+    super(studentAttr);
+    this.previousBackground = studentAttr.previousBackground;
+    this.className = studentAttr.className;
+    this.favSubjects = studentAttr.favSubjects;
+  }
+  listSubjects(favSubjects){
+    return this.favSubjects;
+  }
+  PRAssignment(subject){
+    return `${this.name} has submitted a PR for ${subject}`;
+  }
+  sprintChallenge(subject){
+    return `${this.name} has begun sprint challenge on ${subject}`
+  }
 }
+
 
 /*
   TASK 6
